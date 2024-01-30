0997e24caf820f22b89592d55076c77618dea997
MALU <contact@andantissimo.jp>
2024-01-31 08:25:15 +0900

    PHP 8.2.15

--- php-fpm.rb
+++ php-fpm.rb
@@ -2,9 +2,9 @@ class PhpFpm < Formula
   desc "General-purpose scripting language"
   homepage "https://www.php.net/"
   # Should only be updated if the new version is announced on the homepage, https://www.php.net/
-  url "https://www.php.net/distributions/php-8.2.12.tar.xz"
-  mirror "https://fossies.org/linux/www/php-8.2.12.tar.xz"
-  sha256 "e1526e400bce9f9f9f774603cfac6b72b5e8f89fa66971ebc3cc4e5964083132"
+  url "https://www.php.net/distributions/php-8.2.15.tar.xz"
+  mirror "https://fossies.org/linux/www/php-8.2.15.tar.xz"
+  sha256 "eca5deac02d77d806838275f8a3024b38b35ac0a5d9853dcc71c6cbe3f1f8765"
   license "PHP-3.01"
 
   option "with-ffi", "use ffi"
